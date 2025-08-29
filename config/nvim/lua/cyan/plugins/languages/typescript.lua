5c932e4ee9ca95052dca9062f95b770dc72ca251
Chenxin Yan <yanchenxin2004@gmail.com>
2025-08-28 23:28:00 -0400

    feat(nvim): add tsc and tailwind-tools plugin

---
+++ config/nvim/lua/cyan/plugins/languages/typescript.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    'dmmulroy/tsc.nvim',
+    cmd = { 'TSC' },
+    opts = {},
+    keys = {
+      { '<leader>ck', '<cmd>TSC<CR>', desc = 'Check TypeScript error' },
+    },
+  },
+  {
+    'luckasRanarison/tailwind-tools.nvim',
+    name = 'tailwind-tools',
+    build = ':UpdateRemotePlugins',
+    event = { 'BufReadPre', 'BufNewFile' },
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter',
+    },
+    opts = {}, -- your configuration
+  }
+}
