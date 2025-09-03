c8c960d3bfd6b4073b0936defd113fdc07c1e0e4
Chenxin Yan <yanchenxin2004@gmail.com>
2025-09-03 04:17:59 -0400

    fix(nvim): fix keymap config error

--- config/nvim/lua/cyan/plugins/coding/conform.lua
+++ config/nvim/lua/cyan/plugins/coding/conform.lua
@@ -12,43 +12,44 @@ return { -- Autoformat
       desc = '[F]ormat buffer',
     },
     {
-    'n', '<leader>uf', function()
-  if vim.g.disable_autoformat then
-    vim.g.disable_autoformat = false
-  else
-    vim.g.disable_autoformat = true
-  end
-end, desc = 'Toggle [F]ormat'
-    }
+      '<leader>uf',
+      function()
+        if vim.g.disable_autoformat then
+          vim.g.disable_autoformat = false
+        else
+          vim.g.disable_autoformat = true
+        end
+      end,
+      desc = 'Toggle [F]ormat',
+    },
   },
   opts = {
-      formatters = {},
-      notify_on_error = true,
-      format_on_save = function(bufnr)
-        -- Disable with a global or buffer-local variable
-        if vim.g.disable_autoformat then
-          return
-        end
+    formatters = {},
+    notify_on_error = true,
+    format_on_save = function(bufnr)
+      -- Disable with a global or buffer-local variable
+      if vim.g.disable_autoformat then
+        return
+      end
 
-        -- Disable "format_on_save lsp_fallback" for languages that don't
-        -- have a well standardized coding style. You can add additional
-        -- languages here or re-enable it for the disabled ones.
-        local disable_filetypes = {}
-        local lsp_format_opt
-        if disable_filetypes[vim.bo[bufnr].filetype] then
-          lsp_format_opt = 'never'
-        else
-          lsp_format_opt = 'fallback'
-        end
-        return {
-          timeout_ms = 500,
-          lsp_format = lsp_format_opt,
-        }
-      end,
-      formatters_by_ft = {},
-    },
+      -- Disable "format_on_save lsp_fallback" for languages that don't
+      -- have a well standardized coding style. You can add additional
+      -- languages here or re-enable it for the disabled ones.
+      local disable_filetypes = {}
+      local lsp_format_opt
+      if disable_filetypes[vim.bo[bufnr].filetype] then
+        lsp_format_opt = 'never'
+      else
+        lsp_format_opt = 'fallback'
+      end
+      return {
+        timeout_ms = 500,
+        lsp_format = lsp_format_opt,
+      }
+    end,
+    formatters_by_ft = {},
+  },
   config = function(_, opts)
     require('conform').setup(opts)
   end,
 }
-
