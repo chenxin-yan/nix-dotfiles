5a42a0bded6f845ba67f43159e8168f3f7a1503a
Chenxin Yan <yanchenxin2004@gmail.com>
2025-11-15 23:41:43 -0500

    build(nvim): update neovim config

--- config/nvim/lua/cyan/plugins/extras/avante.lua
+++ config/nvim/lua/cyan/plugins/extras/avante.lua
@@ -1,21 +1,15 @@
 return {
   'yetone/avante.nvim',
   build = vim.fn.has 'win32' ~= 0 and 'powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false' or 'make',
+  enabled = false,
   event = 'VeryLazy',
   version = false, -- Never set this value to "*"! Never!
   ---@module 'avante'
   ---@type avante.Config
   opts = {
     instructions_file = 'AGENT.md',
-    provider = 'opencode',
     behaviour = {
       enable_fastapply = true, -- Enable Fast Apply feature
-    },
-    acp_providers = {
-      ['opencode'] = {
-        command = 'opencode',
-        args = { 'acp' },
-      },
     },
   },
   dependencies = {
