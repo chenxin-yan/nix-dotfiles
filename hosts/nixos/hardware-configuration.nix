396502fbb72731f087d999455a5290d5567ac478
Chenxin Yan <yanchenxin2004@gmail.com>
2025-11-26 01:30:47 -0500

    feat(nixos): add logitech hardware support

--- hosts/nixos/hardware-configuration.nix
+++ hosts/nixos/hardware-configuration.nix
@@ -55,4 +55,7 @@
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+
+  hardware.logitech.wireless.enable = true;
+  hardware.logitech.wireless.enableGraphical = true;
 }
