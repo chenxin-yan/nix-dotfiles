f8645c065cddd33accdd3c1e83399ca3d6728afa
Chenxin Yan <yanchenxin2004@gmail.com>
2025-11-30 04:34:44 -0500

    feat: support chinese font

--- hosts/nixos/configuration.nix
+++ hosts/nixos/configuration.nix
@@ -127,6 +127,8 @@
   # Installing fonts
   fonts.packages = with pkgs; [
     noto-fonts
+    noto-fonts-cjk-sans
+    noto-fonts-cjk-serif
     noto-fonts-color-emoji
     nerd-fonts.jetbrains-mono
   ];
@@ -135,9 +137,19 @@
   fonts.enableDefaultPackages = true;
   fonts.enableGhostscriptFonts = true;
   fonts.fontconfig.defaultFonts = {
-    serif = [ "Noto Serif" ];
-    sansSerif = [ "Noto Sans" ];
-    monospace = [ "JetBrainsMono Nerd Font" ];
+    serif = [
+      "Noto Serif"
+      "Noto Serif CJK SC"
+    ];
+    sansSerif = [
+      "Noto Sans"
+      "Noto Sans CJK SC"
+    ];
+    monospace = [
+      "JetBrainsMono Nerd Font"
+      "Noto Sans Mono CJK SC"
+    ];
+    emoji = [ "Noto Color Emoji" ];
   };
 
   programs.nh = {
