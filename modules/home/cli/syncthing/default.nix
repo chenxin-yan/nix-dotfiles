948f894be377530238e1ecab0bdb6897f86b1847
Chenxin Yan <yanchenxin2004@gmail.com>
2025-12-27 18:04:54 -0500

    feat: add syncthing device config

--- modules/home/cli/syncthing/default.nix
+++ modules/home/cli/syncthing/default.nix
@@ -17,37 +17,51 @@
       overrideDevices = true;
       overrideFolders = true;
 
-      # Global settings
       settings = {
         options = {
-          # Disable usage reporting
           urAccepted = -1;
-          # Enable local discovery
           localAnnounceEnabled = true;
         };
+
+        devices = {
+          "macbook" = {
+            id = "VVPBX7E-COCNGXP-WASVT2N-HU4M2C2-N3E7MZT-LZQMA2R-T7EF55R-WFADDQM";
+          };
+          "nixos" = {
+            id = "A7ARLLU-2SID46Y-XVXR2KS-UX4H5RY-OGWNAUK-ZHPQN5E-SG5DPEH-G76JUAH";
+          };
+        };
+
+        folders = {
+          "dev" = {
+            path = "~/dev";
+            devices = [
+              "macbook"
+              "nixos"
+            ];
+            ignorePerms = false;
+          };
+          "notes" = {
+            path = "~/notes";
+            devices = [
+              "macbook"
+              "nixos"
+            ];
+            ignorePerms = false;
+          };
+        };
       };
+    };
 
-      # Uncomment and configure devices when you have device IDs
-      # settings.devices = {
-      #   "macbook" = {
-      #     id = "XXXXXXX-XXXXXXX-XXXXXXX-XXXXXXX-XXXXXXX-XXXXXXX-XXXXXXX-XXXXXXX";
-      #   };
-      #   "nixos-desktop" = {
-      #     id = "XXXXXXX-XXXXXXX-XXXXXXX-XXXXXXX-XXXXXXX-XXXXXXX-XXXXXXX-XXXXXXX";
-      #   };
-      # };
-
-      # Uncomment and configure folders to sync
-      # settings.folders = {
-      #   "Documents" = {
-      #     path = "~/Documents";
-      #     devices = [ "macbook" "nixos-desktop" ];
-      #   };
-      #   "Notes" = {
-      #     path = "~/Notes";
-      #     devices = [ "macbook" "nixos-desktop" ];
-      #   };
-      # };
+    # Create .stignore files to exclude .DS_Store and node_modules
+    home.file = {
+      "dev/.stignore".text = ''
+        .DS_Store
+        node_modules
+      '';
+      "notes/.stignore".text = ''
+        .DS_Store
+      '';
     };
   };
 }
