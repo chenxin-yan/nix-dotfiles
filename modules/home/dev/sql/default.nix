4498f6b6226ebdc9732657148a6b2652f139a2ab
Chenxin Yan <yanchenxin2004@gmail.com>
2026-05-18 21:46:20 -0700

    fix: sqlit config

--- modules/home/dev/sql/default.nix
+++ modules/home/dev/sql/default.nix
@@ -10,12 +10,14 @@
     dev.sql.enable = lib.mkEnableOption "enables sql tools";
   };
 
-  config = lib.mkIf config.dev.python.enable {
+  config = lib.mkIf config.dev.sql.enable {
     home.packages = with pkgs; [
       sqlit-tui
 
       # editor
       sqlfluff
     ];
+
+    xdg.configFile."sqlit/settings.json".source = ./config/settings.json;
   };
 }
