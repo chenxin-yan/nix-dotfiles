9ba5076602f2e42f1f36ff5d345716fc1dad5d8a
Chenxin Yan <yanchenxin2004@gmail.com>
2026-08-28 10:53:49 -0700

    build: update dependencies

--- modules/home/app/darwin/sketchybar/config/plugins/front_app.sh
+++ modules/home/app/darwin/sketchybar/config/plugins/front_app.sh
@@ -6,7 +6,7 @@
 # https://felixkratz.github.io/SketchyBar/config/events#events-and-scripting
 
 if [ "$SENDER" = "front_app_switched" ]; then
-  source "$CONFIG_DIR/plugins/icon_map.sh"
+  source "$HOME/.config/sketchybar/plugins/icon_map.sh"
   __icon_map "$INFO"
-  sketchybar --set $NAME label="$INFO" icon="$icon_result"
+  sketchybar --set "$NAME" label="$INFO" icon="$icon_result"
 fi
