fa7a9a49b9ede2025df837b622fafa94c4f59c80
Chenxin Yan <yanchenxin2004@gmail.com>
2026-09-15 14:43:06 -0700

    chore: update nvim cmp keymap

--- config/nvim/lua/cyan/plugins/editor/blink-cmp.lua
+++ config/nvim/lua/cyan/plugins/editor/blink-cmp.lua
@@ -93,7 +93,7 @@ return {
       -- See the full "keymap" documentation for information on defining your own keymap.
       keymap = {
         preset = 'none',
-        ['<C-n>'] = {
+        ['<C-k>'] = {
           function(cmp)
             cmp.show { providers = { 'snippets' } }
           end,
@@ -102,8 +102,8 @@ return {
         ['<C-e>'] = { 'hide' },
         ['<C-y>'] = { 'select_and_accept' },
 
-        ['<C-k>'] = { 'select_prev', 'fallback' },
-        ['<C-j>'] = { 'select_next', 'fallback' },
+        ['<C-p>'] = { 'select_prev', 'fallback' },
+        ['<C-n>'] = { 'select_next', 'fallback' },
 
         ['<C-b>'] = { 'scroll_documentation_up', 'fallback' },
         ['<C-f>'] = { 'scroll_documentation_down', 'fallback' },
