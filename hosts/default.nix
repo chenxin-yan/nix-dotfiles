814ad94d9a2a9d1643948aec29d5fb8e691065ed
Chenxin Yan <yanchenxin2004@gmail.com>
2026-09-22 16:04:59 -0700

    feat: set up work macbook

--- hosts/default.nix
+++ hosts/default.nix
@@ -1,7 +1,3 @@
-# Registered machines. The attribute name is the flake target, the managed
-# hostname (networking.hostName) and the argument to `just switch <target>`.
-# Register only real machines; per-host account/state facts live in
-# hosts/<name>/configuration.nix and hosts/<name>/home.nix.
 {
   macbook = {
     system = "aarch64-darwin";
--- hosts/work-macbook/home.nix
+++ hosts/work-macbook/home.nix
@@ -1,3 +1,5 @@
+{ pkgs, ... }:
+
 {
   imports = [
     ../../profiles/home/darwin.nix
@@ -5,5 +7,14 @@
 
   home.stateVersion = "25.05";
 
+  home.packages = with pkgs; [
+    colima
+    docker
+    docker-compose
+    docker-buildx
+  ];
+
+  cli.podman.enable = false;
+  programs.lazydocker.enable = true;
   cli.syncthing.enable = false;
 }
