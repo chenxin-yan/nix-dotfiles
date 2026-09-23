9cb1aadb0005c10a8e97988439d570d3cf2bab70
Chenxin Yan <yanchenxin2004@gmail.com>
2026-09-22 20:54:04 -0700

    build: update dependencies

--- modules/home/agents/sources.nix
+++ modules/home/agents/sources.nix
@@ -11,8 +11,8 @@
   mattpocockSkills = pkgs.fetchFromGitHub {
     owner = "mattpocock";
     repo = "skills";
-    rev = "959a8e9f1edc3adbe2f7e3054bb6fbefa6696260";
-    hash = "sha256-AbIlPEE0VWJq+NJpa56SDzhM8o7vXDBtlJHS5FCTElE=";
+    rev = "c55ee46073ed923f86ce59a5eb3b6d895095d1b7";
+    hash = "sha256-L3CpIT2DeI+fUFl9fcygojtQo2DzEen69rMD1XqR1vM=";
   };
 
   pstack = pkgs.fetchFromGitHub {
@@ -32,7 +32,7 @@
   humanlayerSkills = pkgs.fetchFromGitHub {
     owner = "humanlayer";
     repo = "skills";
-    rev = "3c2629142c5d437428269b1b722b08c0b87f574d";
-    hash = "sha256-lJvu9CGAN/+dzmzck0CodRXn/p7GUkCbfyZxys4nIoU=";
+    rev = "ca7c8088db69e315a8b2deea43820270457f8f3c";
+    hash = "sha256-BX9k5S3hwgik7AKxssUVm7VQRTjgjXVVcE2Jph88tS0=";
   };
 }
--- modules/home/cli/yazi/default.nix
+++ modules/home/cli/yazi/default.nix
@@ -8,8 +8,8 @@ let
   yazi-plugins = pkgs.fetchFromGitHub {
     owner = "yazi-rs";
     repo = "plugins";
-    rev = "58c4f4e2f4835cc9bf6751f39e3f7c574fc7f55a";
-    hash = "sha256-kwf9+KXOL5JXGDoEGdtwq+JujP8GVoOwDgz76FBM3xk=";
+    rev = "f703392df78b5fba5e8f9f1ad0b1cb6d3def9736";
+    hash = "sha256-O1yYAhsf7xMqUrTTSLac06WSxCvUQqedH3DWqGwn/Ok=";
   };
 in
 {
