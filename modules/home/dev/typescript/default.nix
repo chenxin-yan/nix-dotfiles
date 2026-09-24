9dcee8fd6a35301757e955fd14fd3e159dcf8f2e
Chenxin Yan <yanchenxin2004@gmail.com>
2026-09-23 19:57:45 -0700

    build: update dependnecies

--- modules/home/dev/typescript/default.nix
+++ modules/home/dev/typescript/default.nix
@@ -26,14 +26,5 @@
 
     programs.bun.enable = true;
     home.sessionPath = [ "$HOME/.bun/bin" ];
-
-    # Refuse to install package versions younger than 1 day, mitigating
-    # supply-chain attacks that rely on installing a just-published release.
-    # npm's equivalent (min-release-age=1) lives in ~/.npmrc, which holds an
-    # auth token and is therefore not managed here.
-    programs.bun.settings.install.minimumReleaseAge = 86400; # seconds
-    xdg.configFile."pnpm/config.yaml".text = ''
-      minimumReleaseAge: 1440 # minutes
-    '';
   };
 }
